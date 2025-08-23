abbd67417664539d8ea6305d64660e61cefce8d6
Elkhan Isayev <is.elxan@gmail.com>
2025-08-23 14:08:55 +0400

    feat: update NotFoundView navigation links to redirect to home instead of dashboard

--- src/views/custom_error_pages/NotFoundView.vue
+++ src/views/custom_error_pages/NotFoundView.vue
@@ -9,18 +9,11 @@
       </p>
 
       <div class="error-actions">
-        <button @click="goBack" class="btn btn-secondary">
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" fill="currentColor"/>
-          </svg>
-          Go Back
-        </button>
-
-        <router-link to="/dashboard" class="btn btn-primary">
+        <router-link to="/dashboard/home" class="btn btn-primary">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="currentColor"/>
           </svg>
-          Go to Dashboard
+          Go to Home
         </router-link>
       </div>
 
